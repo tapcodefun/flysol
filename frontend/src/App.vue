c89fd2dbdbebbfda1369a99505cfea1b1d928667
FishCode <fishcode@qq.com>
2025-02-23 21:01:49 +0800

    1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -63,6 +63,11 @@
               <el-input v-model="formData.desc" />
             </el-form-item>
           </el-col>
+          <el-col  :span="24" v-if="formData.token">
+            <el-form-item label="安全码" prop="token">
+              <el-input v-model="formData.token" />
+            </el-form-item>
+          </el-col>
           <el-col  :span="24">
             <el-form-item label="私钥" prop="siyao">
               <div style="margin-top: 5px;"> 
